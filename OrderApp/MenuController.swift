17cc3c92646eeedc65459ea1748fe7792fff7416
marie <dul.sreyneang@gmail.com>
2022-06-02 02:40:54 +0800

    Parse the Response
    -check status code is not 200
    -categories need to be decoded into a CategoriesResponse Object
    -return categories from CategoriesResponse object

--- OrderApp/MenuController.swift
+++ OrderApp/MenuController.swift
@@ -22,6 +22,16 @@ class MenuController {
     func fetchCategories() async throws -> [String] {
         let categoriesURL = baseURL.appendingPathComponent("categories")
         let (data, response) = try await URLSession.shared.data(from: categoriesURL)
+        
+        guard let httpRespose = response as? HTTPURLResponse,
+              httpRespose.statusCode == 200 else {
+            throw MenuControllerError.categoriesNotFound
+        }
+        
+        let decoder = JSONDecoder()
+        let categoriesRespose = try decoder.decode(CategoryResponse.self, from: data)
+        
+        return categoriesRespose.categories
     }
         
     func fetchMenuItems(forCategory categoryName: String) async throws -> [MenuItem] {
@@ -35,14 +45,17 @@ class MenuController {
     typealias MinutesToPrepare = Int
 
     func submitOrder(forMenuIDs menuIDs: [Int]) async throws -> MinutesToPrepare {
-        let menuIdsDict = ["menuIds": menuIDs]
-        let jsonEncoder = JSONEncoder()
-        let jsonData = try? jsonEncoder.encode(menuIdsDict)
-        
         let orderURL = baseURL.appendingPathComponent("order")
         var request = URLRequest(url: orderURL)
         request.httpMethod = "POST"
         request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+        
+        let menuIdsDict = ["menuIds": menuIDs]
+        let jsonEncoder = JSONEncoder()
+        let jsonData = try? jsonEncoder.encode(menuIdsDict)
+        request.httpBody = jsonData
+        
+        let (data, response) = try await URLSession.shared.data(for: request)
         
         
     }
