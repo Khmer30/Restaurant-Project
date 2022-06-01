984c7fe88514cee959e08b5bcf30b14d6dee54d7
marie <dul.sreyneang@gmail.com>
2022-06-02 03:29:09 +0800

    -create cell for each row to display -number of row and cells is equal to the number of times -clean up category string by capitalizing it when display in cell

--- OrderApp/CategoryTableViewController.swift
+++ OrderApp/CategoryTableViewController.swift
@@ -9,7 +9,7 @@ import UIKit
 @MainActor
 class CategoryTableViewController: UITableViewController {
     let menuController = MenuController()
-    var categorues = [String]()
+    var categories = [String]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -24,7 +24,7 @@ class CategoryTableViewController: UITableViewController {
         }
         
         func updateUI(with categories: [String]) {
-            self.categorues = categories
+            self.categories = categories
             self.tableView.reloadData()
         }
         
@@ -47,24 +47,27 @@ class CategoryTableViewController: UITableViewController {
 
     override func numberOfSections(in tableView: UITableView) -> Int {
         // #warning Incomplete implementation, return the number of sections
-        return 0
+        return 1
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
-        return 0
+        return categories.count
     }
 
-    /*
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
-
-        // Configure the cell...
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Category", for: indexPath)
+        configureCell(cell, forCategoryAt: indexPath)
 
         return cell
     }
-    */
-
+    
+    func configureCell(_ cell: UITableViewCell, forCategoryAt indexPath: IndexPath) {
+        let category = categories[indexPath.row]
+        var content = cell.defaultContentConfiguration()
+        content.text = category.capitalized
+        cell.contentConfiguration = content
+    }
     /*
     // Override to support conditional editing of the table view.
     override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
