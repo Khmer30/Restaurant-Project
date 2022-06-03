610205a24c6fd77b39be1fd9819d6bc833dc7698
marie <dul.sreyneang@gmail.com>
2022-06-03 09:23:10 +0800

    View and Edit Order

--- OrderApp/OrderTableViewController.swift
+++ OrderApp/OrderTableViewController.swift
@@ -8,6 +8,8 @@
 import UIKit
 
 class OrderTableViewController: UITableViewController {
+    
+    var order = Order()
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -24,18 +26,24 @@ class OrderTableViewController: UITableViewController {
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
-        return 0
+        return order.menuItems.count
     }
 
-    /*
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
-
-        // Configure the cell...
-
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Order", for: indexPath)
+        configure(cell, forItemAt: indexPath)
         return cell
     }
-    */
+    
+    func configure(_ cell: UITableViewCell, forItemAt indexPath: IndexPath) {
+        let menuItem = order.menuItems[indexPath.row]
+        
+        var content = cell.defaultContentConfiguration()
+        content.text = menuItem.name
+        content.secondaryText = menuItem.price.formatted(.currency(code: "usd"))
+        cell.contentConfiguration = content
+    }
+   
 
     /*
     // Override to support conditional editing of the table view.
