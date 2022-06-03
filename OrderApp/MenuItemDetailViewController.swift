21b9dc5ceeeb1a9626c47625223ae8629bdd12ef
marie <dul.sreyneang@gmail.com>
2022-06-03 09:10:39 +0800

    Customize the Button

--- OrderApp/MenuItemDetailViewController.swift
+++ OrderApp/MenuItemDetailViewController.swift
@@ -37,4 +37,12 @@ class MenuItemDetailViewController: UIViewController {
         priceLabel.text = menuItem.price.formatted(.currency(code: "usd"))
         detailTextLabel.text = menuItem.detailText
     }
+    
+    @IBAction func addToOrderButtonTapped(_ sender: UIButton) {
+        UIView.animate(withDuration: 0.5, delay: 0, usingSpringWithDamping: 0.7, initialSpringVelocity: 0.1, options: [], animations: {
+            self.addToOrderButton.transform = CGAffineTransform(scaleX: 2.0, y: 2.0)
+            self.addToOrderButton.transform = CGAffineTransform(scaleX: 1.0, y: 1.0)
+        }, completion: nil)
+    }
+    
 }
