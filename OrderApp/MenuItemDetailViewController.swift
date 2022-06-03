0540e472a2bed598b4e8a357ed7aa06f98511057
marie <dul.sreyneang@gmail.com>
2022-06-03 08:58:58 +0800

    Menu Details
    -add image view and 3 labels and order button

--- OrderApp/MenuItemDetailViewController.swift
+++ OrderApp/MenuItemDetailViewController.swift
@@ -9,6 +9,12 @@ import UIKit
 @MainActor
 class MenuItemDetailViewController: UIViewController {
 
+    @IBOutlet var nameLabel: UILabel!
+    @IBOutlet var imageView: UIImageView!
+    @IBOutlet var priceLabel: UILabel!
+    @IBOutlet var detailTextLabel: UILabel!
+    @IBOutlet var addToOrderButton: UIButton!
+    
     let menuItem: MenuItem
 
     init?(coder: NSCoder, menuItem: MenuItem) {
@@ -22,6 +28,13 @@ class MenuItemDetailViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        updateUI()
 
+    }
+    
+    func updateUI() {
+        nameLabel.text = menuItem.name
+        priceLabel.text = menuItem.price.formatted(.currency(code: "usd"))
+        detailTextLabel.text = menuItem.detailText
     }
 }
