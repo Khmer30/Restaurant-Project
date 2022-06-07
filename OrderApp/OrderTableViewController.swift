7b5feb2f580f3da50e595c0c67d3274792d1da58
marie <dul.sreyneang@gmail.com>
2022-06-08 07:15:18 +0800

    Update the order Badge

--- OrderApp/OrderTableViewController.swift
+++ OrderApp/OrderTableViewController.swift
@@ -15,16 +15,17 @@ class OrderTableViewController: UITableViewController {
         }
     }
 
-//    override func viewDidLoad() {
-//        super.viewDidLoad()
-//
-//        NotificationCenter.default.addObserver(tableView!, selector: #selector(UITableView.reloadData), name: MenuController.orderUpdateNotification, object: nil)
-//    }
-    
-    override func viewDidAppear(_ animated: Bool) {
-//        print(MenuController.shared.order.menuItems.count)
-        tableView.reloadData()
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        navigationItem.leftBarButtonItem = editButtonItem
+
+        NotificationCenter.default.addObserver(tableView!, selector: #selector(UITableView.reloadData), name: MenuController.orderUpdateNotification, object: nil)
     }
+    
+//    override func viewDidAppear(_ animated: Bool) {
+//        print(MenuController.shared.order.menuItems.count)
+//        tableView.reloadData()
+//    }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
@@ -45,27 +46,23 @@ class OrderTableViewController: UITableViewController {
         content.secondaryText = menuItem.price.formatted(.currency(code: "usd"))
         cell.contentConfiguration = content
     }
-   
-
-    /*
+    
     // Override to support conditional editing of the table view.
+    
     override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
         // Return false if you do not want the specified item to be editable.
         return true
     }
-    */
 
-    /*
     // Override to support editing the table view.
     override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
         if editingStyle == .delete {
             // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
+            MenuController.shared.order.menuItems.remove(at: indexPath.row)
+            
+            tableView.reloadData()
+        }
     }
-    */
 
     /*
     // Override to support rearranging the table view.
